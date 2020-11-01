3145f1935e7bf16ff83f77aefd0eb8e24e0a31ed
Cenut Team <45029967+cenut@users.noreply.github.com>
2020-11-02 02:58:02 +0700

    Update util.go

--- util/util.go
+++ util/util.go
@@ -6,8 +6,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/abschain-develop/go-abs/common"
+	"github.com/abschain-develop/go-abs/common/math"
 )
 
 var Ether = math.BigPow(10, 18)
